f2df154ea9b90e829120daa50f01afdd9cd3894c
Jay Nabonne <jaynabonne@gmail.com>
2020-05-19 09:05:44 +0100

    Added trust auth connector.

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -3,7 +3,7 @@ import sbt._
 object AppDependencies {
   import play.core.PlayVersion
 
-  val compile = Seq(
+  val compile: Seq[ModuleID] = Seq(
     play.sbt.PlayImport.ws,
     "org.reactivemongo" %% "play2-reactivemongo"            % "0.18.6-play26",
     "uk.gov.hmrc"       %% "logback-json-logger"            % "4.6.0",
@@ -12,17 +12,19 @@ object AppDependencies {
     "uk.gov.hmrc"       %% "play-ui"                        % "8.8.0-play-26",
     "uk.gov.hmrc"       %% "play-conditional-form-mapping"  % "1.2.0-play-26",
     "uk.gov.hmrc"       %% "bootstrap-play-26"              % "1.4.0",
+    "com.typesafe.play" %% "play-json-joda"                 % "2.7.4",
     "uk.gov.hmrc"       %% "play-whitelist-filter"          % "3.1.0-play-26"
   )
 
-  val test = Seq(
+  val test: Seq[ModuleID] = Seq(
     "org.scalatest"               %% "scalatest"          % "3.0.7",
     "org.scalatestplus.play"      %% "scalatestplus-play" % "3.1.2",
     "org.pegdown"                 %  "pegdown"            % "1.6.0",
     "org.jsoup"                   %  "jsoup"              % "1.10.3",
     "com.typesafe.play"           %% "play-test"          % PlayVersion.current,
     "org.mockito"                 %  "mockito-all"        % "1.10.19",
-    "org.scalacheck"              %% "scalacheck"         % "1.14.0"
+    "org.scalacheck"              %% "scalacheck"         % "1.14.0",
+    "com.github.tomakehurst"      % "wiremock-standalone" % "2.17.0"
   ).map(_ % Test)
 
   def apply(): Seq[ModuleID] = compile ++ test
@@ -30,7 +32,7 @@ object AppDependencies {
   val akkaVersion = "2.5.23"
   val akkaHttpVersion = "10.0.15"
 
-  val overrides = Seq(
+  val overrides: Seq[ModuleID] = Seq(
     "com.typesafe.akka" %% "akka-stream" % akkaVersion,
     "com.typesafe.akka" %% "akka-protobuf" % akkaVersion,
     "com.typesafe.akka" %% "akka-slf4j" % akkaVersion,
