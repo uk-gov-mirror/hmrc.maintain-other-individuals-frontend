8557574c43e2d2a37f9b913a6800308834264ed4
CarolineS-QA <62263396+CarolineS-QA@users.noreply.github.com>
2021-06-14 16:18:39 +0100

    TRUS-4145 [CS] upgrade bootstrap and sbt-auto-build

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -19,7 +19,7 @@ object AppDependencies {
     "com.typesafe.play" %% "play-json-joda"                 % "2.7.4",
     "uk.gov.hmrc"       %% "play-ui"                        % "9.0.0-play-27",
     "uk.gov.hmrc"       %% "play-conditional-form-mapping"  % "1.6.0-play-26",
-    "uk.gov.hmrc"       %% "bootstrap-frontend-play-27"     % "4.1.0",
+    "uk.gov.hmrc"       %% "bootstrap-frontend-play-27"     % "5.3.0",
     "uk.gov.hmrc"       %% "play-language"                  % "4.10.0-play-27"
   )
 
@@ -32,7 +32,7 @@ object AppDependencies {
     "com.typesafe.play"           %% "play-test"              % PlayVersion.current,
     "org.mockito"                 %  "mockito-all"            % "1.10.19",
     "org.scalacheck"              %% "scalacheck"             % "1.14.3",
-    "com.github.tomakehurst"      % "wiremock-standalone"     % "2.25.1"
+    "com.github.tomakehurst"      % "wiremock-standalone"     % "2.27.2"
   ).map(_ % "it, test")
 
   def apply(): Seq[ModuleID] = compile ++ unusedRoutesImportSilencer ++ test
