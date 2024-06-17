bf25cc2f15b956fc7096dc9075d622b1fc35d21f
Adam George <151747054+adageorg@users.noreply.github.com>
2024-06-17 11:39:44 +0100

    DDCE-5540: Updating to use play-frontend-hmrc version 9

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -7,7 +7,7 @@ object AppDependencies {
   private lazy val compile = Seq(
     "uk.gov.hmrc.mongo"       %% "hmrc-mongo-play-30"                     % mongoVersion,
     "uk.gov.hmrc"             %% "bootstrap-frontend-play-30"             % bootstrapVersion,
-    "uk.gov.hmrc"             %% "play-frontend-hmrc-play-30"             % "8.5.0",
+    "uk.gov.hmrc"             %% "play-frontend-hmrc-play-30"             % "9.11.0",
     "uk.gov.hmrc"             %% "domain-play-30"                         % "9.0.0",
     "uk.gov.hmrc"             %% "play-conditional-form-mapping-play-30"  % "2.0.0"
   )
@@ -19,7 +19,6 @@ object AppDependencies {
     "org.scalatestplus"           %% "scalacheck-1-17"          % "3.2.18.0",
     "org.scalatestplus"           %% "mockito-5-10"             % "3.2.18.0",
     "org.jsoup"                   %  "jsoup"                    % "1.17.2",
-    "org.wiremock"                %  "wiremock-standalone"      % "3.5.4",
     "io.github.wolfendale"        %% "scalacheck-gen-regexp"    % "1.1.0",
     "com.vladsch.flexmark"        %  "flexmark-all"             % "0.64.8"
   ).map(_ % Test )
